8c7c9cc1f27e7e84016f8d54d8c52dff00070635
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2025-04-14 19:48:07 +0530

    update

--- js1/script.js
+++ js1/script.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+function test1 (fnumber , snumber) {
+    var z = fnumber + snumber;
+    console.log("first Number is "+ fnumber + " Second Number is "+ snumber + " Sum is " + z); 
+    return z;
+}
+
+let out = test1(34, 56);
+console.log(out);
+
+test1(45, 78);
+test1(65, 34);
+test1(23, 89);
